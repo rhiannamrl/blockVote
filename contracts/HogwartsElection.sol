bce586b99abb0b3fa47aa8c75b3954aaf03c0573
rhianna larocque <rhiannamrl@gmail.com>
2019-01-19 12:00:02 -0500

    feat: add hogwarts contract

---
+++ contracts/HogwartsElection.sol
@@ -0,0 +1,48 @@
+pragma solidity 0.5.0;
+
+//define smart contract
+contract HogwartsElection {
+    //define the structure of the candidate 
+    //candidate variable
+    struct Candidate {
+        uint id;
+        string name;
+        string house;
+        uint voteCount;
+    }
+
+    mapping(address => bool) public voters;
+
+    //create multiple candidates
+    mapping(uint => Candidate) public candidates;
+
+    //declare candidatesVotes variable 
+    uint public candidateId;
+
+    //function to add candidates to the ballot 
+    function addCandidate(string memory _name, string memory _house) private {
+        candidateId++;
+        candidates[candidateId] = Candidate(candidateId, _name, _house, 0);
+    }
+
+    //Constructor adds candidates with add candidate function
+    constructor () public {
+        addCandidate("Hermione Granger", "Gryffindor");
+        addCandidate("Draco Malfoy", "Slytherin");
+        addCandidate("Neville Longbottom", "Gryffindor");
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender], "you can't vote twice, who do you think you are Lord Voldemort?!");
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidateId, "this candidate doesn't exist!");
+
+        // record voting status
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
+}
